8f6de12f503ef9766d27f663e1182c35ab29624f
Ken Chong <thienpow@gmail.com>
2019-09-23 11:57:07 +0800

    theme with provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,42 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_shop/pages/home.dart';
 import 'package:flutter_shop/pages/settings.dart';
+import './theme.dart';
 
 void main() => runApp(
+  
   MultiProvider(
       providers: [
         
       ],
       child: App(),
-    ),
+  ),
+  
 );
 
 class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider<ThemeChanger>(
+      builder: (_) => ThemeChanger(ThemeData.dark()),
+      child: new MaterialAppWithTheme(),
+    );
+  }
+}
+
+class MaterialAppWithTheme extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeChanger>(context);
+
     return MaterialApp(
-      title: 'Flutter Shopping Cart Sample',
-      theme: ThemeData(
-        //primaryColor: Colors.white,
-        //scaffoldBackgroundColor: Colors.white,
-        //canvasColor: Colors.black,
-        primarySwatch: Colors.red,
-      ),
+      theme: theme.getTheme(),
+
       routes: {
         '/': (context) => HomePage(),
         '/settings': (context) => SettingsPage(),
       }
-      
     );
   }
 }
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../theme.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    ThemeChanger _themeChanger = Provider.of<ThemeChanger>(context);
+
     return Scaffold(
       appBar: AppBar(),
       body: Container(
-        child: Text('Settings'),
+        child: Column(
+          children: <Widget>[
+            FlatButton(
+                child: Text('Dark Theme'),
+                onPressed: () => _themeChanger.setTheme(ThemeData.dark())),
+            FlatButton(
+                child: Text('Light Theme'),
+                onPressed: () => _themeChanger.setTheme(ThemeData.light())),
+          ],
+        ),
       ),
     );
   }
