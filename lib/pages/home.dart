1e8059c9102c632bde106abf4b69c65017456cca
Ken Chong <thienpow@gmail.com>
2019-09-23 21:00:54 +0800

    tabbar test

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_shop/pages/drawer.dart';
+import 'package:flutter_shop/widgets/drawer.dart';
+
+import 'package:provider/provider.dart';
+import '../theme.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -19,47 +22,114 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    
+    final theme = Provider.of<ThemeChanger>(context);
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text('$_currentIndex'),
       ),
-      body: Center(
-        
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Catalog here.',
+      body: DefaultTabController(
+          initialIndex: 0,
+          
+          length: 3,
+          child: Scaffold(
+            body: TabBarView(physics: NeverScrollableScrollPhysics(), children: [
+                
+              Scaffold(
+                
+                body: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Catalog here.',
+                    ),
+                    Text(
+                      '',
+                      style: Theme.of(context).textTheme.display1,
+                    ),
+                  ],
+                ),
+              ),
+                    
+              Scaffold(
+                
+                body: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Snap here.',
+                    ),
+                    Text(
+                      '',
+                      style: Theme.of(context).textTheme.display1,
+                    ),
+                  ],
+                ),
+              ),
+                    
+              Scaffold(
+                
+                body: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Cart here.',
+                    ),
+                    Text(
+                      '',
+                      style: Theme.of(context).textTheme.display1,
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+
+            bottomNavigationBar: Material(
+              elevation: 24.0,
+              //here we need to handle the color of the tab bar because it doesn't change automatically following the theme.
+              color: theme.getTheme() == ThemeData.dark() ? Colors.black : Colors.blue,
+              shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(8.0),
+              ),
+              child: 
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 32.0),
+                  child: TabBar(
+                    tabs: [
+                      Tab(
+                        icon: Icon(
+                            Icons.home,
+                            size: 28.0,
+                          ),
+                      ),
+                      Tab(
+                        icon: Icon(
+                            Icons.camera,
+                            size: 28.0,
+                          ),
+                      ),
+                      Tab(
+                        icon: Icon(
+                            Icons.shopping_cart,
+                            size: 28.0,
+                          ),
+                      ),
+                    ],
+                  
+                    onTap: _onBarItemTapped,
+                    indicatorColor: Colors.transparent,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    //labelColor: Colors.blueAccent.withOpacity(0.8),
+                    //unselectedLabelColor: Colors.blue,
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('FlutShop'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera),
-            title: Text('Snap'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            title: Text('Cart'),
-          ),
-        ],
-        currentIndex: _currentIndex,
-        //selectedItemColor: Colors.amber[800],
-        onTap: _onBarItemTapped,
-        
-      ),
+      
+      
+
       drawer: AppDrawer(),
     );
   }
