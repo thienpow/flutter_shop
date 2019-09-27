11ccb295d5d86f60fa38fbae58eaa3c17369a600
Ken Chong <thienpow@gmail.com>
2019-09-28 00:08:27 +0800

    roundButtons

--- lib/pages/front.dart
+++ lib/pages/front.dart
@@ -16,13 +16,67 @@ class Front extends StatelessWidget {
           
           children: <Widget>[
             FeaturedCarousel(),
-            //CarouselWithIndicator(),
-            //getCategoryButtons(context),
+            getCategoryButtons(),
             //getTodayHotlist(context),
           ],
         ),
     );
   }
 
+  Widget getRoundButton(String name) {
+    return RawMaterialButton(
+        constraints: BoxConstraints(maxHeight: 52),
+        onPressed: () {},
+        child: Image.asset('assets/images/round/$name.png'),
+        shape: new CircleBorder(),
+        elevation: 4.0,
+        fillColor: Colors.white,
+        //padding: const EdgeInsets.all(0.0),
+      );
+  }
+  Widget getCategoryButtons() {
+    return Container(
+      alignment: FractionalOffset.center,
+          child: Column(
+            
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(flex: 1, child: Container(),),
+                  getRoundButton('allcat'),
+                  SizedBox(width: 10),
+                  getRoundButton('mencloth'),
+                  SizedBox(width: 10),
+                  getRoundButton('womencloth'),
+                  SizedBox(width: 10),
+                  getRoundButton('sports'),
+                  SizedBox(width: 10),
+                  getRoundButton('electronics'),
+                  Expanded(flex: 1, child: Container(),),
+                ]
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(flex: 1, child: Container(),),
+                  getRoundButton('phone'),
+                  SizedBox(width: 10),
+                  getRoundButton('computer'),
+                  SizedBox(width: 10),
+                  getRoundButton('automobiles'),
+                  SizedBox(width: 10),
+                  getRoundButton('jewellery'),
+                  SizedBox(width: 10),
+                  getRoundButton('garden'),
+                  Expanded(flex: 1, child: Container(),),
+              ],)
+            ]
+          ),
+        );
+  }
 }
 
